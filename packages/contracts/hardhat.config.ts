c8c30864fc312edf27089f0a468155c446d38091
Albert Su <albertsu2001@gmail.com>
2024-01-19 17:27:54 -0500

    ✅Add WETH, addd test files, modify hardhat config

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.5.5",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10",
+      },
+      {
+        version: "0.5.5",
+      },
+    ],
+  },
 };
 
 export default config;
